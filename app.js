b1b006e170ef18b672879a0563ec7f773248c3be
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2023-03-09 01:23:21 +0300

    added a solution to the task in two more ways

--- app.js
+++ app.js
@@ -8,16 +8,51 @@ const personalMovieDB = {
   privat: false
 };
 
-for(let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних фильмов?', ''),
-        b = prompt('Ваша оценка филма?', '');
+// for(let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних фильмов?', ''),
+//         b = prompt('Ваша оценка филма?', '');
 
-  if (a !== '' && b !== '' && a !== null & b !== null && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-  } else {
-    i--;
-  }
+//   if (a !== '' && b !== '' && a !== null & b !== null && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//   } else {
+//     i--;
+//   }
+// }
+
+// Решение циклом while с помощью или ||
+
+// let i = 0;
+// while (i < 2) {
+//   const a = prompt('Один из посдних просмотренных фильмов?', ''),
+//         b = prompt('Ваша оценка фильма?', '');
+
+// if (a === '' || b === '' || a === null || b === null || a.length > 50) {
+//   console.log('error');
+//   i--;
+// } else {
+//   personalMovieDB.movies[a] = b;
+//   console.log('done');
+// }
+// i++;
+// }
+
+// решение циклом do while
+let i = 0;
+
+do {
+  const a = prompt('Последний просмотренный фильм?', ''),
+        b = prompt('Оцените просмотренный фильм?', '');
+
+if (a !== '' && b !== ''&& a !== null && b !== null && a.length < 50) {
+  personalMovieDB.movies[a] = b;
+  console.log('done');
+} else {
+  i--;
+  console.log('error');
 }
+i++;
+} while (i < 2);
+
 
 if (personalMovieDB.count < 10) {
   console.log('Просмотренно слишком мало фильмов');
